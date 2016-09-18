b6d43f56c45e48e9bb409cbc1e39e23bed6b7b52
liswen <327372731@qq.com>
2016-09-18 17:53:09 +0800

    修改document。ready 延迟500

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,23 @@
-window.onload=function () {
+$(document).ready(function(){
+    function an() {
 
-       $("#mainLeft>img").animate({
-           "top":"-400px"
-       },2000)
+        $("#mainLeft>img").animate({
+            "top": "-400px"
+        }, 2000)
         $("#top").animate({
-            "opacity":"0.9"
-        },2000)
+            "opacity": "0.9"
+        }, 2000)
         $("#mainLeft>P").animate({
-            "opacity":"1",
-            "font-size":"10px"
-        },2000)
+            "opacity": "1",
+            "font-size": "10px"
+        }, 2000)
         $("#mainLeft>p").hover(function () {
-            $(this).css("-webkit-transform","rotateY(180deg)")
-        },function () {
-            $(this).css("-webkit-transform","rotateY(0deg)")
+            $(this).css("-webkit-transform", "rotateY(180deg)")
+        }, function () {
+            $(this).css("-webkit-transform", "rotateY(0deg)")
         })
+
+    }
+
+    setTimeout(an, 500)
 })
